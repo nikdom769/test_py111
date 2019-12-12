b9d028a3c0f90f2618229618b46b8cd2cb8bfb7e
NIKDOM <NIKDOM@avalon.ru>
2019-12-12 21:01:23 +0300

    complite task: ao_my_stack and a3_check_brackets

--- Tasks/a0_my_stack.py
+++ Tasks/a0_my_stack.py
@@ -3,42 +3,44 @@ My little Stack
 """
 from typing import Any
 
+memory_steck = []
 
 def push(elem: Any) -> None:
 	"""
 	Operation that add element to stack
-
 	:param elem: element to be pushed
 	:return: Nothing
 	"""
-	print(elem)
+	global memory_steck
+	memory_steck.append(elem)
+	#print(f"Добавлен {elem} в стек")
 	return None
 
 
 def pop() -> Any:
 	"""
 	Pop element from the top of the stack. If not elements - should return None.
-
 	:return: popped element
 	"""
-	return None
+	global memory_steck
+	return memory_steck.pop() if memory_steck else None
 
 
 def peek(ind: int = 0) -> Any:
 	"""
 	Allow you to see at the element in the stack without popping it.
-
 	:param ind: index of element (count from the top, 0 - top, 1 - first from top, etc.)
 	:return: peeked element or None if no element in this place
 	"""
+	global memory_steck
 	print(ind)
-	return None
-
+	return memory_steck[(len(memory_steck)-1) - ind] if memory_steck and ind < len(memory_steck) else None
 
 def clear() -> None:
 	"""
 	Clear my stack
-
 	:return: None
 	"""
+	global memory_steck
+	memory_steck = []
 	return None
--- Tasks/a3_check_brackets.py
+++ Tasks/a3_check_brackets.py
@@ -1,9 +1,16 @@
-def check_brackets(brackets_row: str) -> bool:
-	"""
-	Check whether input string is a valid bracket sequence
-	Valid examples: "", "()", "()()(()())", invalid: "(", ")", ")("
-	:param brackets_row: input string to be checked
-	:return: True if valid, False otherwise
-	"""
+"""
+Check whether input string is a valid bracket sequence
+Valid examples: "", "()", "()()(()())", invalid: "(", ")", ")("
+:param brackets_row: input string to be checked
+:return: True if valid, False otherwise
+"""
 
-	return False
+
+def check_brackets(brackets_row: str) -> bool:
+	cnt: int = 0
+	if len(brackets_row) == 0 or (brackets_row[0] == "(" and len(brackets_row) % 2 == 0):
+		for ch in brackets_row:
+			cnt += 1 if ch == "(" else (-1)
+		return True if cnt == 0 else False
+	else:
+		return False
