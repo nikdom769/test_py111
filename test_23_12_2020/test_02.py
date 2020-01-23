ab4ed573a9ae527b3f23d5659e9fad1559bf706e
nikdom769 <56304864+nikdom769@users.noreply.github.com>
2020-01-23 23:28:28 +0300

    update test_02

--- test_23_12_2020/test_02.py
+++ test_23_12_2020/test_02.py
@@ -8,39 +8,18 @@
 
 def count_man_old(n: int, k: int) -> int:
     man = [i + 1 for i in range(n)]
-    print(man)
     while len(man) > 1:
-        if len(man) < k and len(man) > 1:
-            j = k - len(man)
-            print(j)
-            man.pop(j - 1)
+        j = 0
+        if k < len(man)::
+            j = k - 1
+            man.pop(j)
+        elif k == len(man):
+            man.pop()
         else:
-            man.pop(k - 1)
-    #print(man[0])
+            j = k - len(man) -1
+            man.pop(j)
+        man = man[j:] + man[0:j]
     return man[0]
-
-
-def count_man(n: int, k: int) -> int:
-    man = [i + 1 for i in range(n)]
-    print(man)
-    j = 0 # позиция участника игры
-    while len(man) > 1:
-        if len(man) < k and len(man) > 1:
-            j += k - len(man)
-            #print(j)
-            man.pop(j - 1)
-        else:
-            if j + k - 1 < len(man):
-                j += k - 1
-                man.pop(j)
-            else:
-                j = j + k - 1 - len(man)
-                man.pop(j)
-
-    #print(man[0])
-    return man[0]
-
-
 
 
 if __name__ == "__main__":
